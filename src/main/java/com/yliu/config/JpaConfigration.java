72361870dedfa0e4b9d2ed915c5555aea0b8a0ed
LiuYonghua <yliu19930120@163.com>
2019-08-17 20:57:26 +0800

    增加自动注入分页参数的配置

--- src/main/java/com/yliu/config/JpaConfigration.java
+++ src/main/java/com/yliu/config/JpaConfigration.java
@@ -3,10 +3,12 @@ package com.yliu.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @Configuration
 @EnableJpaRepositories("com.yliu.dao.*")
 @EnableMongoAuditing
+@EnableSpringDataWebSupport
 public class JpaConfigration {
 	
 }
